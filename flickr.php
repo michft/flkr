b3d254a886ebff2395a61f03c9c114c417afb5cf
Michael Tomkins <michft@gmail.com>
2012-09-12 08:11:47 +1000

    Go

--- flickr.php
+++ flickr.php
@@ -1,1 +1,26 @@
-a
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Michael's PHP test response.</title>
+    <meta name="author" content="Michael Tomkins" />
+    <meta name="robots" content="all" />
+    <meta name="rights-standard" content="content;cc:by-nc" />
+    <!-- <link rel="icon" type="image/png" href="http://mich431.net/siteimage.png">
+    <link type="text/css" rel="stylesheet" href="http://mich431.net/css/site.css">
+    <base href="http://www.mich431.net/"> -->
+</head>
+<body>
+    <div id="outer">
+        <div id="inner">
+            <div id="content">
+  
+ 
+
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
+
