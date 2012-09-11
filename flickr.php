a2871d71c882c29fea7dc20499c74586986c7bf3
Michael Tomkins <michft@gmail.com>
2012-09-12 09:52:33 +1000

    Unclosed div fix

--- flickr.php
+++ flickr.php
@@ -14,8 +14,37 @@
     <div id="outer">
         <div id="inner">
             <div id="content">
-  
- 
+                <?php
+// Subject of email sent to you.
+$subject = 'Camp form from ' . htmlspecialchars($_POST['searchTerm']);
+echo $subject;
+// Your email address. This is where the form information will be sent.
+$emailadd = 'm@mich431.net';
+
+// Where to redirect after form is processed.
+$url = 'http://mich431.net/index.html';
+
+// Makes all fields required. If set to '1' no field can not be empty. If set to '0' any or all fields can be empty.
+$req = '0';
+
+
+                    $text = "Results from form:\n\n";
+                    $space = ' ';
+                    $line = '
+';
+                    $csv = '';
+                    foreach ($_POST as $key => $value){
+                        $value = htmlspecialchars($value);
+                        if ($req == '1'){
+                            if ($value == '')
+                                {echo "$key is empty";die;}
+                        }
+                        $j = strlen($key);
+                        if ($j >= 80)
+                            {echo "Name of form element $key cannot be longer than 80 characters";die;}
+                        echo "<br />" .  $value;
+                    }
+                ?>
 
             </div>
         </div>
